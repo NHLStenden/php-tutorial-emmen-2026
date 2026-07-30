b6e85a4bb7c4f5aff31f1c428e270ce18a6fc14e
Martin Molema <martin.molema@nhlstenden.com>
2026-07-30 10:26:04 +0200

    Improved HTML after W3C validation

--- Docker/websites/examples/week01/06/index.php
+++ Docker/websites/examples/week01/06/index.php
@@ -9,8 +9,6 @@ $surname = "Molema";
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP Example 2</title>
     <style>body {
