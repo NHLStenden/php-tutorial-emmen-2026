e3a6f2626a9982904dcfd4fb59bd85b55837bedd
Martin Molema <Martin.Molema@nhlstenden.com>
2026-09-22 12:13:06 +0200

    fix wrong variable name

--- Docker/websites/examples/week04/11/index.php
+++ Docker/websites/examples/week04/11/index.php
@@ -1,15 +1,15 @@
 <pre><code>
 <?php
 
-$x = 10;
+$x = 10.0;
 $y = 12;
 $z = 1.2;
 var_dump($x, $y, $z);
 
 
 $name = "Martin Molema";
-$characters = explode(' ', $name);
-print_r($characters);
+$words = explode(' ', $name);
+print_r($words);
 
 echo "x is null? : "    . (is_null($x)  ? "yes" : "no") . "\n";
 echo "x is integer? : " . (is_int($x)   ? "yes" : "no") . "\n";
